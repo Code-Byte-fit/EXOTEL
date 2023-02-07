0d34ca62defe78b40d1257123ef7aec87f9da343
Shamly-Shanawaz <iamshamly@gmail.com>
2023-02-07 11:47:50 +0530

    initial table structures

---
+++ server/models/MiniBarItems.js
@@ -0,0 +1,29 @@
+module.exports=(sequelize,Datatypes)=>{
+    const MiniBarItems=sequelize.define("MiniBarItems",{
+        FirstName:{
+            type:Datatypes.STRING,
+            allowNull:false,
+        },
+        LastName:{
+            type:Datatypes.STRING,
+            allowNull:false,
+        },
+        BirthDate:{
+            type:Datatypes.DATEONLY,
+            allowNull:false,
+        },
+        Country:{
+            type:Datatypes.STRING,
+            allowNull:false,
+        },
+        Email:{
+            type:Datatypes.STRING,
+            allowNull:false,
+        },
+        PhoneNumber:{
+            type:Datatypes.STRING,
+            allowNull:false,
+        },
+    })
+    return MiniBarItems;
+}
