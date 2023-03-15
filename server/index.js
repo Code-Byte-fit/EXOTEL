199c5c8bcd28486fd2159d41b2b6e69148e19732
Kavindya60 <nethmikavindya60@gmail.com>
2023-03-15 16:21:09 +0530

    Revert "task allocation and viewing"
    This reverts commit 13e240e9510ecc5904adf3fd4c806cf2d66428c2.

--- server/index.js
+++ server/index.js
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 const express=require("express")
 const cors=require('cors')
 
@@ -7,19 +6,9 @@ app.use(express.json());
 app.use(cors());
 
 const db=require('./models')
-=======
-const express = require("express");
-const app = express();
-const cors = require("cors");
 
-app.use(express.json());
-app.use(cors());
->>>>>>> Stashed changes
-
-const db = require("./models");
 
 //Routers
-<<<<<<< Updated upstream
 const userRouter=require('./routes/Users')
 app.use("/users",userRouter);
 
@@ -42,22 +31,7 @@ db.sequelize.sync().then(()=>{
     })
 })
 
-=======
-const userRouter = require("./routes/Users");
-app.use("/users", userRouter);
->>>>>>> Stashed changes
 
-const guestRouter = require("./routes/Guests");
-app.use("/guests", guestRouter);
 
-const roomRouter = require("./routes/Rooms");
-app.use("/rooms", roomRouter);
 
-const tasksRouter = require("./routes/Tasks");
-app.use("/tasks", tasksRouter);
 
-db.sequelize.sync().then(() => {
-  app.listen(3001, () => {
-    console.log("server running on port 3001");
-  });
-});
